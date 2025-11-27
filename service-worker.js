ef3d75814669453c933d66923ce554b4c3f083a8
lumitaketime <123154836+lumitaketime@users.noreply.github.com>
2025-11-27 09:32:06 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,22 @@
 const CACHE_NAME = 'aomori-trip-v1';
-const ASSETS_TO_CACHE = [
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  // 如果你有圖片或 CSS，也要列在這裡，例如：
-  // './style.css',
-  // './icon.png'
+  './icon192.png',
+  './icon512.png'
 ];
 
-// 安裝 Service Worker 並快取檔案
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
+// 安裝
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 攔截網路請求，如果快取有就用快取的，沒有就上網抓
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+// 攔截請求 (讓網頁離線也能開)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
